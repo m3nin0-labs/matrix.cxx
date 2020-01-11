19a953349bd00c92f35293940b92effb1255aba2
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-01-11 00:20:39 -0300

    Implementation of matrix operators

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,7 +1,8 @@
+#include <iostream>
 #include "matrix.hpp"
 
 
-MatrixH::Matrix(int ncol, int nrow)
+MatrixH::Matrix::Matrix(int ncol, int nrow)
 {
 	this->ncol = ncol;
 	this->nrow = nrow;
@@ -17,4 +18,76 @@ double MatrixH::Matrix::get(const int col, const int row)
 void MatrixH::Matrix::set(const int col, const int row, double value)
 {
 	data[col * row + col] = value;
+}
+
+MatrixH::Matrix& MatrixH::Matrix::operator +(MatrixH::Matrix& m_obj)
+{
+	return sum(m_obj);
+}
+
+MatrixH::Matrix& MatrixH::Matrix::operator -(MatrixH::Matrix& m_obj)
+{
+	return subtract(m_obj);
+}
+
+MatrixH::Matrix& MatrixH::Matrix::operator *(MatrixH::Matrix& m_obj)
+{
+	return matmul(m_obj);
+}
+
+MatrixH::Matrix& MatrixH::Matrix::operator /(MatrixH::Matrix& m_obj)
+{
+	return divide(m_obj);
+}
+
+MatrixH::Matrix& MatrixH::Matrix::sum(MatrixH::Matrix m_obj)
+{
+	MatrixH::Matrix* m = new MatrixH::Matrix(this->ncol, this->nrow);
+	
+	for(int i = 0; i < this->ncol * this->nrow; i++)
+	{
+		m->data[i] = this->data[i] + m_obj.data[i];
+	}
+	return *m;
+}
+
+MatrixH::Matrix& MatrixH::Matrix::subtract(MatrixH::Matrix m_obj)
+{
+	MatrixH::Matrix* m = new MatrixH::Matrix(this->ncol, this->nrow);
+	
+	for(int i = 0; i < this->ncol * this->nrow; i++)
+	{
+		m->data[i] = this->data[i] - m_obj.data[i];
+	}
+	return *m;
+}
+
+MatrixH::Matrix& MatrixH::Matrix::divide(MatrixH::Matrix m_obj)
+{
+	MatrixH::Matrix* m = new MatrixH::Matrix(this->ncol, this->nrow);
+	
+	for(int i = 0; i < this->ncol * this->nrow; i++)
+	{
+		m->data[i] = this->data[i] / m_obj.data[i];
+	}
+	return *m;
+}
+
+MatrixH::Matrix& MatrixH::Matrix::matmul(MatrixH::Matrix m_obj)
+{
+	MatrixH::Matrix* m = new MatrixH::Matrix(this->ncol, this->nrow);
+	
+	for(int i = 0; i < this->ncol * this->nrow; i++)
+	{
+		m->data[i] = this->data[i] * m_obj.data[i];
+	}
+	return *m;
+}
+
+int main()
+{
+	MatrixH::Matrix m1 = MatrixH::Matrix(1, 1);
+	MatrixH::Matrix m2 = MatrixH::Matrix(2, 2);
+	
+	m1 + m2;
 }
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -12,13 +12,18 @@ namespace MatrixH
     public:
         Matrix(int ncol, int nrow);
 
-        double get(int ncol, int nrow);
-        double set(int ncol, int nrow);
+		double get(int ncol, int nrow);
+        void set(int ncol, int nrow, double value);
+        
+		Matrix& operator +(Matrix& m_obj);
+		Matrix& operator -(Matrix& m_obj);
+		Matrix& operator *(Matrix& m_obj);
+		Matrix& operator /(Matrix& m_obj);
 		
-		Matrix& operator +(const Matrix& m_obj);
-		Matrix& operator -(const Matrix& m_obj);
-		Matrix& operator *(const Matrix& m_obj);
-		Matrix& operator /(const Matrix& m_obj);
+		Matrix& sum(Matrix m_obj);
+		Matrix& subtract(Matrix m_obj);
+		Matrix& divide(Matrix m_obj);
+		Matrix& matmul(Matrix m_obj);
     };
 }
 
