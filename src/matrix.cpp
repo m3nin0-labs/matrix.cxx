c11e2889608424bdfac09a16f5a939d09ca18326
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-01-10 21:31:34 -0300

    Implement matrix base methods

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,3 +1,20 @@
 #include "matrix.hpp"
 
 
+MatrixH::Matrix(int ncol, int nrow)
+{
+	this->ncol = ncol;
+	this->nrow = nrow;
+	
+	this->data = new double[ncol * nrow];
+}
+
+double MatrixH::Matrix::get(const int col, const int row)
+{
+	return this->data[col * row + col];
+}
+
+void MatrixH::Matrix::set(const int col, const int row, double value)
+{
+	data[col * row + col] = value;
+}
