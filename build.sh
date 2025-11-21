bc9ce70831ac3fce268c5d7693db19745aaad6b2
Felipe Carlos <efelipecarlos@gmail.com>
2025-11-20 22:45:10 -0300

    build: add conan as package manager

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-cmake -G "CodeLite - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+conan install . --output-folder=build --build=missing
 
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
